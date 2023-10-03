cb30bd941e167b5136fc7c3c616a5d46073a7dde
missionfloyd <missionfloyd@users.noreply.github.com>
2023-10-02 22:33:03 -0600

    Lint

--- javascript/extraNetworks.js
+++ javascript/extraNetworks.js
@@ -232,7 +232,7 @@ function popup(contents) {
     if (!globalPopup) {
         globalPopup = document.createElement('div');
         globalPopup.classList.add('global-popup');
-        
+
         var close = document.createElement('div');
         close.classList.add('global-popup-close');
         close.addEventListener("click", closePopup);
