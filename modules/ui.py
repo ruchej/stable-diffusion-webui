51fb8dac7bcd1bac6c587171c735a0609acee78c
AUTOMATIC1111 <16777216c@gmail.com>
2024-01-04 19:47:00 +0300

    Have upscale button use the same seed as hires fix.

--- modules/ui.py
+++ modules/ui.py
@@ -485,8 +485,8 @@ def create_ui():
 
             txt2img_outputs = [
                 output_panel.gallery,
+                output_panel.generation_info,
                 output_panel.infotext,
-                output_panel.html_info,
                 output_panel.html_log,
             ]
 
@@ -504,7 +504,7 @@ def create_ui():
             output_panel.button_upscale.click(
                 fn=wrap_gradio_gpu_call(modules.txt2img.txt2img_upscale, extra_outputs=[None, '', '']),
                 _js="submit_txt2img_upscale",
-                inputs=txt2img_inputs[0:1] + [output_panel.gallery, dummy_component] + txt2img_inputs[1:],
+                inputs=txt2img_inputs[0:1] + [output_panel.gallery, dummy_component, output_panel.generation_info] + txt2img_inputs[1:],
                 outputs=txt2img_outputs,
                 show_progress=False,
             )
@@ -517,8 +517,8 @@ def create_ui():
                 inputs=[dummy_component],
                 outputs=[
                     output_panel.gallery,
+                    output_panel.generation_info,
                     output_panel.infotext,
-                    output_panel.html_info,
                     output_panel.html_log,
                 ],
                 show_progress=False,
@@ -846,8 +846,8 @@ def create_ui():
                 ] + custom_inputs,
                 outputs=[
                     output_panel.gallery,
+                    output_panel.generation_info,
                     output_panel.infotext,
-                    output_panel.html_info,
                     output_panel.html_log,
                 ],
                 show_progress=False,
@@ -887,8 +887,8 @@ def create_ui():
                 inputs=[dummy_component],
                 outputs=[
                     output_panel.gallery,
+                    output_panel.generation_info,
                     output_panel.infotext,
-                    output_panel.html_info,
                     output_panel.html_log,
                 ],
                 show_progress=False,
