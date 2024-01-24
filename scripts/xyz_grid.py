5755087c471263384bc4a42c27dc8f917fdab933
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-01-25 05:45:45 +0900

    xyz filter blank for number axes

--- scripts/xyz_grid.py
+++ scripts/xyz_grid.py
@@ -555,6 +555,8 @@ class Script(scripts.Script):
                 valslist_ext = []
 
                 for val in valslist:
+                    if val.strip() == '':
+                        continue
                     m = re_range.fullmatch(val)
                     mc = re_range_count.fullmatch(val)
                     if m is not None:
@@ -577,6 +579,8 @@ class Script(scripts.Script):
                 valslist_ext = []
 
                 for val in valslist:
+                    if val.strip() == '':
+                        continue
                     m = re_range_float.fullmatch(val)
                     mc = re_range_count_float.fullmatch(val)
                     if m is not None:
