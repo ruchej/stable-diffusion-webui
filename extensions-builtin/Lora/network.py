3cb483e40c1eb2f925fc3cc52fc025cfe65b33fe
KohakuBlueleaf <apolloyeh0123@gmail.com>
2024-03-13 11:47:33 +0800

    Add missing .mean

--- extensions-builtin/Lora/network.py
+++ extensions-builtin/Lora/network.py
@@ -185,7 +185,7 @@ class NetworkModule:
         )
 
         dora_merged = (
-            merged_scale1 * (dora_scale / merged_scale1_norm)
+            merged_scale1 / merged_scale1.mean(dim=self.dora_mean_dim, keepdim=True) * self.dora_scale
         )
         final_updown = dora_merged - orig_weight
         return final_updown
